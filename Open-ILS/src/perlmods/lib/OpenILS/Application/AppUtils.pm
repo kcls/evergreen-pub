6f3f7ea699ba1fabee683b028359f5b911259f56
Bill Erickson <berickxx@gmail.com>
2023-09-14 17:15:00 -0400

    JBAS-3129 Mark Damaged Patron Alerting Note
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- Open-ILS/src/perlmods/lib/OpenILS/Application/AppUtils.pm
+++ Open-ILS/src/perlmods/lib/OpenILS/Application/AppUtils.pm
@@ -2660,5 +2660,33 @@ sub marc_xml_to_doc {
     return $marc_doc;
 }
 
+# Penalty creator that can be run within a transaction.
+sub create_penalty_message {
+    my ($class, $e, $pen_type, $user_id, $org_id, $title, $message) = @_;
+
+    my $penalty = Fieldmapper::actor::user_standing_penalty->new;
+    $penalty->usr($user_id);
+    $penalty->org_unit($org_id);
+    $penalty->standing_penalty($pen_type);
+    $penalty->staff($e->requestor->id);
+
+    my $aum = Fieldmapper::actor::usr_message->new;
+    $aum->sending_lib($e->requestor->ws_ou);
+    $aum->title($title);
+    $aum->usr($user_id);
+    $aum->message($message);
+    $aum->pub('f'); # TODO
+
+    $aum = $e->create_actor_usr_message($aum) or return $e->die_event;
+
+    # Link 'em
+    $penalty->usr_message($aum->id);
+
+    $e->create_actor_user_standing_penalty($penalty) or return $e->die_event;
+
+    return $penalty;
+}
+
+
 1;
 
