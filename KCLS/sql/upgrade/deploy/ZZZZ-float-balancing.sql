a196189216dbbb5bf824d963ff5dfcb11dfe2d5d
Bill Erickson <berickxx@gmail.com>
2024-02-28 12:11:11 -0500

    JBAS-3233 Balanced Floating WIP
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- KCLS/sql/upgrade/deploy/ZZZZ-float-balancing.sql
+++ KCLS/sql/upgrade/deploy/ZZZZ-float-balancing.sql
@@ -3,25 +3,22 @@
 
 BEGIN;
 
--- Policies are linked directly to org units, not
--- config.floating_group_member entries, on the assumption that an org
--- unit wants "X items per copy location" or "X max copies per bib per
--- copy location" regardless of the floating group at play.
--- TODO verify max bibs makes sense or if it should be org unit global.
 CREATE TABLE config.org_unit_float_policy (
+    -- Policies are linked directly to org units, not
+    -- config.floating_group_member entries, on the assumption that an org
+    -- unit wants "X items per copy location" or "X max copies per bib per
+    -- copy location" regardless of the floating group at play.
+
     id              SERIAL PRIMARY KEY,
     active          BOOL NOT NULL DEFAULT FALSE,
-    org_unit        INT NOT NULL REFERENCE actor.org_unit(id)
-                    DEFERRABLE INITIALLY DEFERRED ON DELETE CASCADE,
-    max_same_bib    INT,
-    copy_location   INT,
-    items_allowed   INT,
-    CONSTRAINT      one_loc_per_orgc UNIQUE(org_unit, copy_location)
-    CONSTRAINT      needs_some_rules CHECK (
-        max_same_bib IS NOT NULL OR (
-            copy_location IS NOT NULL AND items_allowed IS NOT NULL
-        )
-    )
+    org_unit        INT NOT NULL REFERENCES actor.org_unit(id)
+                    ON DELETE CASCADE DEFERRABLE INITIALLY DEFERRED,
+    max_per_bib     INT,
+    copy_location   INT NOT NULL,
+    max_items       INT, 
+    CONSTRAINT      one_loc_per_orgc UNIQUE(org_unit, copy_location),
+    CONSTRAINT      needs_some_rules CHECK 
+                        (max_per_bib IS NOT NULL OR max_items IS NOT NULL)
 );
 
 CREATE OR REPLACE FUNCTION evergreen.float_members(
@@ -76,6 +73,137 @@ BEGIN
     END LOOP;
 END;
 $FUNK$ LANGUAGE PLPGSQL;
+
+
+
+CREATE MATERIALIZED VIEW evergreen.float_target_counts AS 
+    SELECT 
+        COUNT(acp.id) AS bib_slots_filled,
+        acn.record AS bib_record,
+        acp.circ_lib AS circ_lib,
+        acp.location AS copy_location,
+        acpl.name AS copy_location_name,
+        coufp.max_items AS location_slots,
+        coufp.max_per_bib AS bib_slots
+    FROM asset.copy acp
+    JOIN asset.call_number acn ON acn.id = acp.call_number
+    JOIN config.copy_status ccs ON ccs.id = acp.status
+    JOIN asset.copy_location acpl ON acpl.id = acp.location
+    JOIN config.org_unit_float_policy coufp ON (
+        coufp.org_unit = acp.circ_lib 
+        AND coufp.copy_location = acp.location
+    )
+    WHERE 
+        NOT acp.deleted
+        AND NOT acn.deleted
+        AND acp.call_number > 0
+        AND acn.record > 0
+        AND ccs.is_available
+        AND coufp.active
+    GROUP BY 2, 3, 4, 5, 6, 7
+;
+
+CREATE VIEW evergreen.float_target_location_counts AS 
+    -- Count of copies per location among org units and copy
+    -- locations where float policies apply.
+    SELECT 
+        SUM(ftbc.bib_slots_filled) AS location_slots_filled,
+        ftbc.circ_lib,
+        ftbc.copy_location,
+        ftbc.copy_location_name,
+        ftbc.location_slots,
+    FROM evergreen.float_target_bib_counts ftbc
+    GROUP BY 2, 3, 4, 5
+;
+
+
+CREATE OR REPLACE FUNCTION evergreen.float_destination(
+    copy_id INTEGER,
+	to_ou INTEGER
+) RETURNS INTEGER AS $FUNK$
+DECLARE
+    copy asset.copy%ROWTYPE;
+
+    --- Float policy at the destination/checkin org unit.
+    policy config.org_unit_float_policy%ROWTYPE,
+
+    -- Org units within this copy's float member group.
+    member_orgs: INTEGER[];
+
+    target_bib: INTEGER;
+
+    target_bib_counts: evergreen.float_target_bib_counts%ROWTYPE;
+
+    target_location_counts: evergreen.float_target_location_counts%ROWTYPE;
+
+    -- KCLS matches copy location by their code names since each branch
+    -- maintains its own version of (practically) every copy location, 
+    -- i.e. locations are not shared by the consortium.
+    copy_location_code TEXT;
+BEGIN
+
+    SELECT INTO copy * FROM asset.copy WHERE id = copy_id;
+
+    IF copy.floating IS NULL THEN
+        -- This copy doesn't float.
+        RETURN copy.circ_lib;
+    END IF;
+
+    SELECT INTO member_orgs ARRAY(
+        FROM evergreen.float_members(copy.floating, copy.circ_lib, to_ou));
+
+    IF ARRAY_LENGTH(member_orgs) = 0 THEN
+        -- Likely excluded from floating in this group.
+        RETURN copy.circ_lib;
+    END IF;
+
+    SELECT INTO copy_location_code name FROM asset.copy_location WHERE id = copy.location;
+
+    SELECT INTO target_bib acn.record 
+        FROM asset.copy acp
+        JOIN asset.call_number acn ON acn.id = acp.location
+        WHERE acp.id = copy_id;
+
+    -- Float to the destination (checkin) branch if possible.
+
+    -- Have we exceeded the bib target count at the destination?
+    PERFORM TRUE
+        FROM evergreen.float_target_bib_counts ftbc
+        WHERE ftbc.circ_lib = to_ou
+            AND ftbc.bib_record = target_record
+            AND ftbc.copy_location_name = copy_location_code -- KCLS
+            AND ftbc.bib_slots_filled < ftbc.bib_slots;
+
+    IF FOUND THEN
+        -- We have not exceeded the bib slots at the target destination.
+        -- Have we exceeded the copy location slots?
+        PERFORM TRUE 
+            FROM evergreen.float_target_location_counts ftlc
+            WHERE ftlc.circ_lib = to_ou
+                AND ftlc.copy_location_name = copy_location_code -- KCLS
+                AND ftlc.location_slots_filled < ftlc.location_slots;
+
+        IF FOUND THEN
+            -- We have room at the destination/checkin branch.  Float to it.
+            RETURN to_ou;
+        END IF;
+    END IF;
+
+    -- No room at the inn... Find the best float target.
+
+END;
+$FUNK$ LANGUAGE PLPGSQL;
+
+
+------------------------------------------------------------------------------
+-- make some sample data
+INSERT INTO config.org_unit_float_policy 
+    (active, org_unit, max_per_bib, copy_location, max_items)
+SELECT TRUE, aou.id, 2, acpl.id, 10
+FROM actor.org_unit aou
+JOIN asset.copy_location acpl ON acpl.owning_lib = aou.id;
+
+REFRESH MATERIALIZED VIEW evergreen.float_target_bib_counts;
 
 COMMIT;
 
--- KCLS/sql/upgrade/revert/ZZZZ-float-balancing.sql
+++ KCLS/sql/upgrade/revert/ZZZZ-float-balancing.sql
@@ -2,6 +2,11 @@
 
 BEGIN;
 
--- XXX Add DDLs here.
+DROP FUNCTION IF EXISTS evergreen.float_members;
+
+DROP VIEW IF EXISTS evergreen.float_target_location_counts;
+DROP MATERIALIZED VIEW IF EXISTS evergreen.float_target_counts;
+
+DROP TABLE IF EXISTS config.org_unit_float_policy;
 
 COMMIT;
