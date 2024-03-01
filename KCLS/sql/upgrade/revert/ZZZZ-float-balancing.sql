f344d36253518eebae1e951532f685da95edf50d
Bill Erickson <berickxx@gmail.com>
2024-03-01 11:16:06 -0500

    JBAS-3233 Balanced Floating WIP
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- KCLS/sql/upgrade/revert/ZZZZ-float-balancing.sql
+++ KCLS/sql/upgrade/revert/ZZZZ-float-balancing.sql
@@ -6,6 +6,8 @@ DROP FUNCTION IF EXISTS kcls.float_members;
 
 DROP FUNCTION IF EXISTS kcls.float_target_has_bib_slot(INTEGER, TEXT, INTEGER);
 DROP MATERIALIZED VIEW IF EXISTS kcls.float_target_counts;
+
+DROP VIEW IF EXISTS kcls.float_balanced_items;
 DROP VIEW IF EXISTS kcls.on_shelf_float_balanced_items;
 DROP VIEW IF EXISTS kcls.in_transit_float_balanced_items;
 
