2a39b9b15571bd093018d276a3e9b24a48efe34a
Bill Erickson <berickxx@gmail.com>
2024-06-07 10:12:37 -0400

    6468753385 All-Access Register Email WIP
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

---
+++ KCLS/sql/upgrade/deploy/XXXX-pending-patrons-notice.sql
@@ -0,0 +1,26 @@
+-- Deploy kcls-evergreen:pending-patrons-notice to pg
+-- requires: 0004-damaged-item-letter
+
+BEGIN;
+
+DO $INSERT$ BEGIN IF evergreen.insert_on_deploy() THEN                         
+
+INSERT INTO action_trigger.event_definition (
+    id, active, owner, name, hook, validator, reactor, 
+    delay, granularity, retention_interval 
+) VALUES (
+    262,
+    TRUE,
+    1,
+    'All-Access Registration Email',
+    'stgu.created',
+    NOOP_True,
+    NOOP_True,
+    '00:00:00',
+    'All-Access-Register-Email',
+    '1 year',
+)
+
+END IF; END $INSERT$;                                                          
+
+COMMIT;
