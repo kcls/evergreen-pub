37fa00229e4687a43a8a932b115d26f4e34cd1ee
Bill Erickson <berickxx@gmail.com>
2024-07-12 10:48:01 -0400

    JBAS-2858 Support for max active patron requests
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- KCLS/patron-ui/src/app/requests/requests.service.ts
+++ KCLS/patron-ui/src/app/requests/requests.service.ts
@@ -1,17 +1,23 @@
 import {Injectable, EventEmitter} from '@angular/core';
-import {Gateway} from '../gateway.service';
+import {Gateway, Hash} from '../gateway.service';
 import {AppService} from '../app.service';
+import {Settings} from '../settings.service';
 
 @Injectable()
 export class RequestsService {
     selectedFormat: string | null = null;
     illOptOut = false;
     requestsAllowed: boolean | null = null;
+    activeRequestCount = 0;
+    maxRequestCount = 0;
 
     // Emits after completion of every new patron auth+permission check.
     patronChecked: EventEmitter<void> = new EventEmitter<void>();
 
-    constructor(private app: AppService, private gateway: Gateway) {
+    constructor(
+        private app: AppService,
+        private settings: Settings,
+        private gateway: Gateway) {
         app.authSessionLoad.subscribe(() => this.checkRequestPerms());
     }
 
@@ -22,6 +28,7 @@ export class RequestsService {
 
     checkRequestPerms() {
         this.requestsAllowed = null;
+        this.activeRequestCount = 0;
 
         this.gateway.requestOne(
             'open-ils.actor',
@@ -29,8 +36,30 @@ export class RequestsService {
             this.app.getAuthtoken()
         ).then((r: unknown) => {
             this.requestsAllowed = Number(r) === 1;
+        }).then(() => {
+            return this.settings.getServerSetting('patron_requests.max_active')
+            .then(r => this.maxRequestCount = Number(r));
+        }).then(() => {
+            if (!this.requestsAllowed) {
+                return Promise.resolve([]);
+            }
+            return this.gateway.requestOne(
+                'open-ils.actor',
+                'open-ils.actor.patron-request.retrieve.pending',
+                this.app.getAuthtoken()
+            );
+        }).then((r: unknown) => {
+            this.activeRequestCount = (r as Hash[]).length;
+            if (this.tooManyActiveRequests()) {
+                this.requestsAllowed = false;
+            }
             this.patronChecked.emit();
         });
+    }
+
+    tooManyActiveRequests(): boolean {
+        return this.activeRequestCount > 0 &&
+            this.activeRequestCount >= this.maxRequestCount;
     }
 }
 
