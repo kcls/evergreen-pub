7d10ce9dee20fb19777d05621521f44c3bd3ac83
Bill Erickson <berickxx@gmail.com>
2024-09-05 13:11:08 -0400

    JBAS-3134 Auto-refunds exported as JSON
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- KCLS/utility-scripts/refunds/transfer-refunds.sh
+++ KCLS/utility-scripts/refunds/transfer-refunds.sh
@@ -4,10 +4,10 @@ SCP_DEST=$1
 
 [ -z "$SCP_DEST" ] && echo "SCP destination required" && exit 1;
 
-for csv_file in /openils/var/data/auto-refunds/refund-$TODAY*; do
-    if [ -e $csv_file ]; then
-        echo "Transfering refund file: $csv_file to $SCP_DEST"
-        scp $csv_file $SCP_DEST
+for json_file in /openils/var/data/auto-refunds/refund-$TODAY*; do
+    if [ -e $json_file ]; then
+        echo "Transfering refund file: $json_file to $SCP_DEST"
+        scp $json_file $SCP_DEST
     fi;
 done;
 
