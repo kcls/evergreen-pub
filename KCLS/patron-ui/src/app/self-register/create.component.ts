e63fa634e2ab308ea05cf977053d9ec91c7fd094
Bill Erickson <berickxx@gmail.com>
2024-09-23 11:11:35 -0400

    6332897196 Self-Reg Angular
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- KCLS/patron-ui/src/app/self-register/create.component.ts
+++ KCLS/patron-ui/src/app/self-register/create.component.ts
@@ -5,11 +5,19 @@ import {Gateway, Hash} from '../gateway.service';
 import {AppService} from '../app.service';
 import {SelfRegisterService} from './register.service';
 
+const JUV_AGE = 18; // years
+
 @Component({
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.scss']
 })
 export class SelfRegisterCreateComponent implements OnInit {
+
+    minDob = new Date("1900-01-01");
+    maxDob = new Date();
+
+    isJuvenile = false;
+    juvMinDob: Date;
 
     cardDesignGroup = this.formBuilder.group({
         design: ['', Validators.required],
@@ -19,13 +27,25 @@ export class SelfRegisterCreateComponent implements OnInit {
         delivery: ['Mail', Validators.required],
     });
 
+    contactsGroup = this.formBuilder.group({
+    });
+
+    addressGroup = this.formBuilder.group({
+    });
+
+    submitGroup = this.formBuilder.group({
+    });
+
     nameGroup = this.formBuilder.group({
         first: ['', Validators.required],
         middle: '',
         last: ['', Validators.required],
-        legal_first: '',
-        legal_middle: '',
-        legal_last: '',
+        legalIsSame: true,
+        legalFirst: '',
+        legalMiddle: '',
+        legalLast: '',
+        dob: ['', Validators.required],
+        guardian: '',
     });
 
     constructor(
@@ -34,9 +54,23 @@ export class SelfRegisterCreateComponent implements OnInit {
         private formBuilder: FormBuilder,
         public app: AppService,
         public requests: SelfRegisterService,
-    ) {}
+    ) {
+        this.juvMinDob = new Date();
+        this.juvMinDob.setFullYear(new Date().getFullYear() - JUV_AGE);
+    }
 
     ngOnInit() {
+
+
+        this.nameGroup.controls.dob.valueChanges.subscribe((dob: unknown) => {
+            if ((dob as Date) > this.juvMinDob) {
+                this.isJuvenile = true;
+                this.nameGroup.controls.guardian.setValidators(Validators.required);
+            } else {
+                this.isJuvenile = false;
+                this.nameGroup.controls.guardian.clearValidators();
+            }
+        });
     }
 }
 
