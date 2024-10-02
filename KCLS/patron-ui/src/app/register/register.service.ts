fac6f2d0434b743766b5002de337405801e358cb
Bill Erickson <berickxx@gmail.com>
2024-10-02 12:32:31 -0400

    6332897196 Self-Reg Angular
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

---
+++ KCLS/patron-ui/src/app/register/register.service.ts
@@ -0,0 +1,16 @@
+import {Injectable, EventEmitter} from '@angular/core';
+import {Gateway, Hash} from '../gateway.service';
+import {AppService} from '../app.service';
+import {Settings} from '../settings.service';
+
+@Injectable()
+export class RegisterService {
+	inKioskMode = false;
+
+    constructor(
+        private app: AppService,
+        private settings: Settings,
+        private gateway: Gateway) {
+    }
+}
+
