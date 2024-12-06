455d8e67a7892fd9ee7563cbe1e255afe400f6d9
Bill Erickson <berickxx@gmail.com>
2024-12-06 13:04:23 -0500

    HD21823 Browse entry cleanup repair
    Avoid attempting to delete metabib.browse_entry's that are linked to
    from bibs or auths.  Fixes the previous version of this logic which was
    incomplete.
    
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

---
+++ KCLS/sql/upgrade/deploy/XXXX-metabib-browse-cache-fix.sql
@@ -0,0 +1,54 @@
+-- Deploy kcls-evergreen:XXXX-metabib-browse-cache-fix to pg
+-- requires: 0007-patron-req-cont
+
+BEGIN;
+
+CREATE OR REPLACE FUNCTION metabib.maintain_browse_metabib_fields_cache() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+DECLARE
+    entry_id BIGINT;
+    keep_entry BOOLEAN;
+BEGIN
+
+    SELECT INTO entry_id
+        CASE WHEN TG_OP = 'DELETE' THEN OLD.entry ELSE NEW.entry END;
+    
+    -- distinct list of config.metabib_field IDs which link 
+    -- to a given metabib.browse_entry via bib or auth maps.
+    UPDATE metabib.browse_entry
+    SET metabib_fields_cache = ARRAY(
+        SELECT DISTINCT(x.def_id) FROM (
+            SELECT mbedm.def AS def_id
+                FROM metabib.browse_entry_def_map mbedm
+                WHERE mbedm.entry = entry_id
+            UNION
+            SELECT map.metabib_field AS def_id
+                FROM metabib.browse_entry_simple_heading_map mbeshm
+                JOIN authority.simple_heading ash ON (mbeshm.simple_heading = ash.id)
+                JOIN authority.control_set_auth_field_metabib_field_map_refs map
+                    ON (ash.atag = map.authority_field)
+                WHERE mbeshm.entry = entry_id
+j       )x
+    )
+    WHERE id = entry_id;
+
+    -- While we're here, remove orphaned browse entries
+    -- At this point, browse entries mapped to the deleted bibs/auths
+    -- have already been removed.
+    IF TG_OP = 'DELETE' THEN
+        SELECT INTO keep_entry EXISTS (
+            SELECT TRUE FROM metabib.browse_entry_def_map WHERE entry = entry_id LIMIT 1
+            UNION
+            SELECT TRUE FROM metabib.browse_entry_simple_heading_map WHERE entry = entry_id LIMIT 1
+        );
+        IF NOT keep_entry THEN
+            DELETE FROM metabib.browse_entry WHERE id = entry_id;
+        END IF;
+    END IF;
+
+    RETURN NEW;
+END;
+$$;
+
+COMMIT;
