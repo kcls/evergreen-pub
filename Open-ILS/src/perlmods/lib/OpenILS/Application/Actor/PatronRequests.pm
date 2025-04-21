449e81905937dbe4c15848cc6a838e8b6820aa60
Bill Erickson <berickxx@gmail.com>
2025-04-21 12:19:47 -0400

    8875496417 PR staff UI overhaul filters
    Migrate from canned grid filters/sorting to a custom API to improve user
    experience.
    
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- Open-ILS/src/perlmods/lib/OpenILS/Application/Actor/PatronRequests.pm
+++ Open-ILS/src/perlmods/lib/OpenILS/Application/Actor/PatronRequests.pm
@@ -810,4 +810,139 @@ sub ill_requests_allowed_impl {
 }
 
 
+__PACKAGE__->register_method(
+    method   => 'search_requests',
+    api_name => 'open-ils.actor.patron.patron-request.search',
+    stream => 1,
+    signature => {
+        params => [
+            {desc => 'Authtoken', type => 'string'},
+            {desc => 'Query', type => 'hash'},
+        ],
+        desc => q/Search for requests/
+    }
+);
+
+sub search_requests {
+    my ($self, $client, $auth, $filters, $order_by, $limit, $offset) = @_;
+    return undef unless $filters;
+
+    $order_by ||= {auir => ['create_date']};
+    $limit ||= 50;
+    $offset ||= 0;
+
+    my $e = new_editor(authtoken => $auth);
+    return $e->event unless $e->checkauth;
+    return $e->event unless $e->allowed('STAFF_LOGIN');
+
+    my $auir_where = {};
+
+    $auir_where->{format} = $filters->{format} if $filters->{format};
+    $auir_where->{language} = $filters->{language} if $filters->{language};
+    $auir_where->{audience} = $filters->{audience} if $filters->{audience};
+
+    my $where = {'+auir' => $auir_where};
+
+    my $query = {
+        select => {auir => ['id']},
+        from => {auir => 'au'},
+        where => $where,
+        order_by => $order_by,
+        limit => $limit,
+        offset => $offset,
+    };
+
+    if ($filters->{claimed_by_me}) {
+        $auir_where->{claimed_by} = $e->requestor->id;
+    } elsif ($filters->{is_claimed}) {
+        $auir_where->{claim_date} = {'<>' => undef};
+    } elsif ($filters->{is_unclaimed}) {
+        $auir_where->{claim_date} = undef;
+    } 
+
+    if ($filters->{is_staff_active}) {
+        $auir_where->{reject_date} = undef;
+
+        $auir_where->{'-or'} = [
+            {'-and' => [{route_to => 'ill'}, {illno => undef}]},
+            {'-and' => [{route_to => 'acq'}, {lineitem => undef}]}
+        ];
+
+    } elsif ($filters->{is_staff_complete}) {
+        $auir_where->{'-or'} = [
+            {reject_date => {'<>' => undef}},
+            {lineitem => {'<>' => undef}},
+            {'-and' => [{route_to => 'ill'}, {complete_date => {'<>' => undef}}]}
+        ];
+    }
+
+    if (my $date = $filters->{create_date}) {
+        my $start = DateTime::Format::ISO8601->parse_datetime(clean_ISO8601($date));
+        my $end = DateTime::Format::ISO8601->parse_datetime(clean_ISO8601($date));
+        $end->add(seconds => 86400);
+        $auir_where->{create_date} = {between => [$start->iso8601, $end->iso8601]};
+    }
+
+    if ($filters->{route_to_acq}) {
+        $auir_where->{route_to} = 'acq';
+    }
+
+    if ($filters->{route_to_ill}) {
+        $auir_where->{route_to} = 'ill';
+    }
+
+    if ($filters->{is_canceled}) {
+        $auir_where->{cancel_date} = {'<>' => undef};
+    }
+
+    if (my $title = $filters->{title}) {
+        $auir_where->{title} = {ilike => "$title%"};
+    }
+
+    if (my $author = $filters->{author}) {
+        $auir_where->{author} = {ilike => "$author%"};
+    }
+
+    if (my $isbn = $filters->{isbn}) {
+        $auir_where->{isbn} = {ilike => "$isbn%"};
+    }
+
+    if (my $name = $filters->{patron_family_name}) {
+        $where->{'+au'} = {
+            '-or' => [
+                {family_name => {ilike => "$name%"}},
+                {pref_family_name => {'ilike' => "$name%"}}
+            ]
+        };
+    }
+
+    my $ids = $e->json_query($query);
+
+    for my $id (map {$_->{id}} @$ids) {
+
+        my $req = $e->retrieve_actor_user_item_request([
+            $id, {
+                flesh => 2,
+                flesh_fields => {
+                    auir => ['usr', 'format', 'language', 'audience'],
+                    au => ['card', 'profile', 'stat_cat_entries']
+                }
+            }
+        ]);
+
+        if ($req->claimed_by) {
+            # avoid fleshing multiple 'au' values above.
+            $req->claimed_by($e->retrieve_actor_user($req->claimed_by));
+        }
+
+        my $stat = request_status_impl($e, $req);
+
+        $client->respond({request => $req, status => $stat->{status}});
+    }
+
+    return undef;
+}
+
+
+
 1;
