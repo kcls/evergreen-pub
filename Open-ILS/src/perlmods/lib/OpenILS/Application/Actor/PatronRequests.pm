e1edca1d22e526ef38ead4f77d5c217652742b96
Bill Erickson <berickxx@gmail.com>
2025-07-02 12:41:17 -0400

    9493807323 PR Staff ILLNO filter
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- Open-ILS/src/perlmods/lib/OpenILS/Application/Actor/PatronRequests.pm
+++ Open-ILS/src/perlmods/lib/OpenILS/Application/Actor/PatronRequests.pm
@@ -916,6 +916,10 @@ sub search_requests {
         $auir_where->{isbn} = {ilike => "$isbn%"};
     }
 
+    if (my $illno = $filters->{illno}) {
+        $auir_where->{illno} = $illno;
+    }
+
     if (my $name = $filters->{patron_family_name}) {
         $where->{'+au'} = {
             '-or' => [
