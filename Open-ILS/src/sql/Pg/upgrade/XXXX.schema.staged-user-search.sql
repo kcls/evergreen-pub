f5d44aa5210a9f3d8164f4c37d071d6a2835ea80
Bill Erickson <berickxx@gmail.com>
2026-04-07 20:28:41 -0400

    6332897196 Staged user search with ilike
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- Open-ILS/src/sql/Pg/upgrade/XXXX.schema.staged-user-search.sql
+++ Open-ILS/src/sql/Pg/upgrade/XXXX.schema.staged-user-search.sql
@@ -1,19 +1,19 @@
 
 BEGIN;
 
-CREATE INDEX staging_user_first_given_name_idx ON staging.user_stage (LOWER(first_given_name));
-CREATE INDEX staging_user_second_given_name_idx ON staging.user_stage (LOWER(second_given_name));
-CREATE INDEX staging_user_family_name_idx ON staging.user_stage (LOWER(family_name));
+CREATE INDEX staging_user_first_given_name_idx ON staging.user_stage USING GIN (first_given_name gin_trgm_ops);
+CREATE INDEX staging_user_second_given_name_idx ON staging.user_stage USING GIN (second_given_name gin_trgm_ops);
+CREATE INDEX staging_user_family_name_idx ON staging.user_stage USING GIN (family_name gin_trgm_ops);
 
-CREATE INDEX staging_user_first_given_name_squashed_idx 
-    ON staging.user_stage (evergreen.unaccent_and_squash(first_given_name));
-CREATE INDEX staging_user_second_given_name_squashed_idx 
-    ON staging.user_stage (evergreen.unaccent_and_squash(second_given_name));
-CREATE INDEX staging_user_family_name_squashed_idx 
-    ON staging.user_stage (evergreen.unaccent_and_squash(family_name));
+CREATE INDEX staging_user_first_given_name_squashed_idx
+    ON staging.user_stage USING GIN (evergreen.unaccent_and_squash(first_given_name) gin_trgm_ops);
+CREATE INDEX staging_user_second_given_name_squashed_idx
+    ON staging.user_stage USING GIN (evergreen.unaccent_and_squash(second_given_name) gin_trgm_ops);
+CREATE INDEX staging_user_family_name_squashed_idx
+    ON staging.user_stage USING GIN (evergreen.unaccent_and_squash(family_name) gin_trgm_ops);
 
-CREATE INDEX staging_user_email_idx ON staging.user_stage (LOWER(email));
-CREATE INDEX staging_user_day_phone_idx ON staging.user_stage (day_phone);
+CREATE INDEX staging_user_email_idx ON staging.user_stage USING GIN (email gin_trgm_ops);
+CREATE INDEX staging_user_day_phone_idx ON staging.user_stage USING GIN (day_phone gin_trgm_ops);
 
 COMMIT;
 
