f5c058cbb86e3e68195ff5acb5e5706778e1c216
Bill Erickson <berickxx@gmail.com>
2026-07-02 14:37:05 -0400

    EVG-45 Aged copy transits table/script
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

---
+++ KCLS/sql/upgrade/revert/0030-aged-copy-transits.sql
@@ -0,0 +1,11 @@
+DROP INDEX CONCURRENTLY IF EXISTS action.transit_copy_root_send_time_idx;
+DROP INDEX CONCURRENTLY IF EXISTS action.transit_copy_prev_hop_idx;
+
+BEGIN;
+
+DROP PROCEDURE IF EXISTS action.age_copy_transits(INTERVAL, INTEGER, INTEGER);
+DROP PROCEDURE IF EXISTS action.age_standalone_copy_transits(INTERVAL, INTEGER, INTEGER, INTEGER);
+DROP FUNCTION IF EXISTS action.age_copy_transit_group(INTEGER);
+DROP TABLE IF EXISTS action.aged_copy_transit;
+
+COMMIT;
