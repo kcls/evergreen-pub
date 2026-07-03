e8a99d20f17ee165d05c1515c50df508fe607900
Bill Erickson <berickxx@gmail.com>
2026-07-03 11:30:40 -0400

    EVG-45 Aged copy transits table/script
    Signed-off-by: Bill Erickson <berickxx@gmail.com>

--- KCLS/utility-scripts/age-copy-transits/age-copy-transits.sh
+++ KCLS/utility-scripts/age-copy-transits/age-copy-transits.sh
@@ -11,25 +11,14 @@ TIMEOUT="SET STATEMENT_TIMEOUT = 36000000"
 
 AGE_AGE=${AGE_AGE:-"5 years"}
 BATCH_LIMIT=${BATCH_LIMIT:-50000}
-BATCH_SIZE=${BATCH_SIZE:-1000}
 
-echo -n "Aging copy transits at "
+echo -n "Aging copy transits (limit=$BATCH_LIMIT) at "
 date +"%F %T"
 
-total=0
+# NOTE the DB function ages each group of transits within its own transaction,
+# so there's no need to loop on batches here to prevent long-running transactions
+# and row locks.
+echo "$TIMEOUT; CALL action.age_copy_transits('$AGE_AGE', $BATCH_LIMIT);" | $PSQL;
 
-while true; do
-    count=$(echo "$TIMEOUT; CALL action.age_copy_transits('$AGE_AGE', $BATCH_SIZE);" \
-        | $PSQL -t -A | tail -1)
-
-    total=$((total + count))
-
-    echo "Aged $count transits ($total total)"
-
-    if [ "$count" -eq 0 ] || [ "$total" -ge "$BATCH_LIMIT" ]; then
-        break
-    fi
-done
-
-echo -n "Done aging $total copy transits at "
+echo -n "Done aging copy transits at "
 date +"%F %T"
